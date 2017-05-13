5f37fbee3e2e0aad826e607f94335e020d28dcdc
Ashish sharma <ashishsharma@Ashishs-Mac-mini.local>
2017-05-13 11:54:50 +0530

    Initial Commit

---
+++ UTMarket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UTMarket
+//
+//  Created by Ashish sharma on 13/05/17.
+//  Copyright © 2017 Ashish sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
